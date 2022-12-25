5aebb588feaacf5e52f2e560d85fa25f0d36bc3a
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-12-25 10:15:47 -0300

    ADD - Login and Logout

--- routes.js
+++ routes.js
@@ -13,5 +13,7 @@ route.get("/", homeController.index,);
 /* LOGIN */
 route.get("/login", loginController.index);
 route.post("/login/register", loginController.register);
+route.post("/login/login", loginController.login);
+route.get("/login/logout", loginController.logout);
 
 module.exports = route;
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -22,6 +22,25 @@ class Login {
         this.user = null;
     }
 
+    async login() {
+        this.validate();
+        if(this.errors.length > 0) return;
+
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if(!this.user) {
+            this.errors.push("User does not exists");
+
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push("Password wrong");
+            this.user = null;
+            return;
+        }
+    }
+
     async register() {
         this.validate();
         if(this.errors.length > 0) return;
@@ -33,16 +52,12 @@ class Login {
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
         
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch(e) {
-            console.log(e);
-        }
+        this.user = await LoginModel.create(this.body);
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({ email: this.body.email });
-        if(user) this.errors.push("User already exists");
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if(this.user) this.errors.push("User already exists");
     }
 
     validate() {
